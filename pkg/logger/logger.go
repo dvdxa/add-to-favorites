cdef6a993b8a86af9d24e9d4809854771977ff6e
dvdxa__ <dovudhaydarov@gmail.com>
2023-08-16 13:26:34 +0500

    reusable

---
+++ pkg/logger/logger.go
@@ -0,0 +1,78 @@
+package logger
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"io"
+	"log"
+	"os"
+	"path"
+	"runtime"
+)
+
+type writeHook struct {
+	Writer    []io.Writer
+	LogLevels []logrus.Level
+}
+
+func (hook *writeHook) Fire(entry *logrus.Entry) error {
+	line, err := entry.String()
+	if err != nil {
+		return err
+	}
+	for _, w := range hook.Writer {
+		_, err := w.Write([]byte(line))
+		if err != nil {
+			return err
+		}
+
+	}
+	return nil
+}
+
+func (hook *writeHook) Levels() []logrus.Level {
+	return hook.LogLevels
+}
+
+var e *logrus.Entry
+
+type Logger struct {
+	*logrus.Entry
+}
+
+func GetLogger() *Logger {
+	return &Logger{e}
+}
+
+func init() {
+	l := logrus.New()
+	l.SetReportCaller(true)
+	l.Formatter = &logrus.TextFormatter{
+		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
+			filename := path.Base(frame.File)
+			return fmt.Sprintf("%s()", frame.Function), fmt.Sprintf("%s:%d", filename, frame.Line)
+		},
+		DisableColors: false,
+		FullTimestamp: true,
+	}
+
+	err := os.MkdirAll("./internal/logs", 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	allFile, err := os.OpenFile("./internal/logs/all.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
+	l.SetOutput(io.Discard)
+
+	l.AddHook(&writeHook{
+		Writer:    []io.Writer{allFile, os.Stdout},
+		LogLevels: logrus.AllLevels,
+	})
+
+	l.SetLevel(logrus.TraceLevel)
+
+	e = logrus.NewEntry(l)
+}
